db84e2d82d1df3baeff9f2eebbdade7e02079bdf
lrraymond13 <lrraymond13@gmail.com>
2014-11-24 13:05:33 -0500

    Added changing directory functionality

--- shell.c
+++ shell.c
@@ -7,13 +7,43 @@
 #include "utils.h"
 
 int main(int argc, char **argv) {
-  char command[] = "ls -a -l";
-  char pipeline[] = "ls -a -l | wc -l";
+	while(1) {
+		//print out shell prompt
+		printf("Sauron ");
+		//get line that you type in 
+		char *line = NULL;
+		int capacity = 0;
+		getline(&line, &capacity, stdin);
+		//print what the user typed in
+		printf("%s\n", line);
+		//actually run the user commmands
 
-  cmd_struct* c = parse_command(command);
-  print_command(c);
+	  	cmd_struct* c = parse_command(line);
+	 	print_command(c);
+		
+		//takes over the process and ends the loop
+		//doesn't start a new processs
+	
+		pid_t pid = fork();
 
-  pipeline_struct* p = parse_pipeline(pipeline);
-  print_pipeline(p);
-  return 0;
+		if (pid == 0) {
+			if (strcmp(c->progname, "cd")==0) {
+				chdir(c->args[1]);
+			}
+			else {
+				execvp(c->progname, c->args); }
+		}
+		else {
+			wait(NULL);
+		}
+
+	}
+
+//  char command[] = "ls -a -l";
+ // char pipeline[] = "ls -a -l | wc -l";
+
+
+ //  pipeline_struct* p = parse_pipeline(pipeline);
+ //  print_pipeline(p);
+ //  return 0;
 }
